3d48eddd23529c23d6483527eb1a9adefe667e75
Isak <isakulstrup@gmail.com>
2024-05-27 12:16:14 +0200

    Changed inventory to an Int

--- src/Codec.elm
+++ src/Codec.elm
@@ -1,4 +1,9 @@
-module Codec exposing (decodeInventory, decodeIslands, encodeInventory, encodeIslands)
+module Codec exposing
+    ( decodeInventory
+    , decodeIslands
+    , encodeInventory
+    , encodeIslands
+    )
 
 import Engine.Inventory as Inventory exposing (Inventory)
 import Engine.Island as Island exposing (Island, Tile)
@@ -6,32 +11,10 @@ import Json.Decode as Decode exposing (Decoder)
 import Json.Encode as Encode
 
 
-itemEncoder : ( String, Int ) -> Encode.Value
-itemEncoder ( itemName, amount ) =
-    Encode.object
-        [ ( "itemName", Encode.string itemName )
-        , ( "amount", Encode.int amount )
-        ]
-
-
-itemDecoder : Decoder ( String, Int )
-itemDecoder =
-    Decode.map2 Tuple.pair
-        (Decode.field "itemName" Decode.string)
-        (Decode.field "amount" Decode.int)
-
-
-inventoryDecoder : Decoder Inventory
-inventoryDecoder =
-    Decode.list itemDecoder
-        |> Decode.map Inventory.fromList
-
-
 encodeInventory : Inventory -> String
 encodeInventory inventory =
     inventory
-        |> Inventory.toList
-        |> Encode.list itemEncoder
+        |> Encode.int
         |> Encode.encode 0
 
 
@@ -39,7 +22,7 @@ decodeInventory : Maybe String -> Inventory
 decodeInventory maybeInput =
     case maybeInput of
         Just inputString ->
-            Decode.decodeString inventoryDecoder inputString
+            Decode.decodeString Decode.int inputString
                 |> Result.withDefault Inventory.empty
 
         Nothing ->
@@ -89,17 +72,6 @@ encodeIslands islands =
     islands
         |> Encode.list islandEncoder
         |> Encode.encode 0
-
-
-
--- decodeInventory : Maybe String -> Inventory
--- decodeInventory maybeInput =
---     case maybeInput of
---         Just inputString ->
---             Decode.decodeString inventoryDecoder inputString
---                 |> Result.withDefault Inventory.empty
---         Nothing ->
---             Inventory.empty
 
 
 decodeIslands : String -> List Island
--- src/Engine/Inventory.elm
+++ src/Engine/Inventory.elm
@@ -2,43 +2,18 @@ module Engine.Inventory exposing
     ( Inventory
     , addItem
     , empty
-    , fromList
-    , toList
     )
 
-import Dict exposing (Dict)
 
-
-type Inventory
-    = Inv (Dict String Int)
+type alias Inventory =
+    Int
 
 
 empty : Inventory
 empty =
-    Inv Dict.empty
+    0
 
 
-addItem : String -> Int -> Inventory -> Inventory
-addItem itemName amount (Inv inventory) =
-    if amount > 0 then
-        (case Dict.get itemName inventory of
-            Just _ ->
-                Dict.update itemName (Maybe.map (\item -> item + amount)) inventory
-
-            Nothing ->
-                Dict.insert itemName amount inventory
-        )
-            |> Inv
-
-    else
-        Inv inventory
-
-
-fromList : List ( String, Int ) -> Inventory
-fromList list =
-    Inv <| Dict.fromList list
-
-
-toList : Inventory -> List ( String, Int )
-toList (Inv inventory) =
-    Dict.toList inventory
+addItem : Int -> Inventory -> Inventory
+addItem item inventory =
+    inventory + (item |> max 0)
