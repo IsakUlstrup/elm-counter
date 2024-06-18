1926f9d0653acc15278fee5dd3e4a401aae5816a
Isak <isakulstrup@gmail.com>
2024-06-18 17:02:09 +0200

    prettier outline filter

--- src/Filters.elm
+++ src/Filters.elm
@@ -7,21 +7,34 @@ import Svg.Attributes
 outline : Svg msg
 outline =
     Svg.filter [ Svg.Attributes.id "outline-filter" ]
-        [ Svg.feMorphology
+        [ Svg.feGaussianBlur
             [ Svg.Attributes.in_ "SourceAlpha"
+            , Svg.Attributes.stdDeviation "3"
+            , Svg.Attributes.result "BLUR"
+            ]
+            []
+        , Svg.feColorMatrix
+            [ Svg.Attributes.in_ "BLUR"
+            , Svg.Attributes.mode "matrix"
+            , Svg.Attributes.values "1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 6 -3"
+            , Svg.Attributes.result "GOO"
+            ]
+            []
+        , Svg.feMorphology
+            [ Svg.Attributes.in_ "GOO"
             , Svg.Attributes.operator "dilate"
             , Svg.Attributes.result "DILATED"
-            , Svg.Attributes.radius "3"
+            , Svg.Attributes.radius "10"
             ]
             []
         , Svg.feFlood
-            [ Svg.Attributes.floodColor "beige"
-            , Svg.Attributes.floodOpacity "1"
-            , Svg.Attributes.result "PINK"
+            [ Svg.Attributes.floodColor "white"
+            , Svg.Attributes.floodOpacity "100"
+            , Svg.Attributes.result "BEIGE"
             ]
             []
         , Svg.feComposite
-            [ Svg.Attributes.in_ "PINK"
+            [ Svg.Attributes.in_ "BEIGE"
             , Svg.Attributes.in2 "DILATED"
             , Svg.Attributes.operator "in"
             , Svg.Attributes.result "OUTLINE"
