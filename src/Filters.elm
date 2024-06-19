43093daf0ae06f3e6b4fe1d45d344ca1f6d557ca
Isak <isakulstrup@gmail.com>
2024-06-19 15:49:34 +0200

    filter adjustments

--- src/Filters.elm
+++ src/Filters.elm
@@ -7,24 +7,18 @@ import Svg.Attributes
 outline : Svg msg
 outline =
     Svg.filter [ Svg.Attributes.id "outline-filter" ]
-        [ Svg.feGaussianBlur
-            [ Svg.Attributes.in_ "SourceAlpha"
-            , Svg.Attributes.stdDeviation "3"
-            , Svg.Attributes.result "BLUR"
+        [ Svg.feMorphology
+            [ Svg.Attributes.in_ "SourceGraphic"
+            , Svg.Attributes.operator "dilate"
+            , Svg.Attributes.result "DILATED"
+            , Svg.Attributes.radius "5"
             ]
             []
         , Svg.feColorMatrix
-            [ Svg.Attributes.in_ "BLUR"
+            [ Svg.Attributes.in_ "DILATED"
             , Svg.Attributes.mode "matrix"
             , Svg.Attributes.values "1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 6 -3"
             , Svg.Attributes.result "GOO"
-            ]
-            []
-        , Svg.feMorphology
-            [ Svg.Attributes.in_ "GOO"
-            , Svg.Attributes.operator "dilate"
-            , Svg.Attributes.result "DILATED"
-            , Svg.Attributes.radius "10"
             ]
             []
         , Svg.feFlood
@@ -35,13 +29,26 @@ outline =
             []
         , Svg.feComposite
             [ Svg.Attributes.in_ "BEIGE"
-            , Svg.Attributes.in2 "DILATED"
+            , Svg.Attributes.in2 "GOO"
             , Svg.Attributes.operator "in"
             , Svg.Attributes.result "OUTLINE"
             ]
             []
+        , Svg.feGaussianBlur
+            [ Svg.Attributes.in_ "OUTLINE"
+            , Svg.Attributes.stdDeviation "3"
+            , Svg.Attributes.result "BLUR"
+            ]
+            []
+        , Svg.feColorMatrix
+            [ Svg.Attributes.in_ "BLUR"
+            , Svg.Attributes.mode "matrix"
+            , Svg.Attributes.values "1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 6 -3"
+            , Svg.Attributes.result "GOO2"
+            ]
+            []
         , Svg.feMerge []
-            [ Svg.feMergeNode [ Svg.Attributes.in_ "OUTLINE" ] []
+            [ Svg.feMergeNode [ Svg.Attributes.in_ "GOO2" ] []
             , Svg.feMergeNode [ Svg.Attributes.in_ "SourceGraphic" ] []
             ]
         ]
